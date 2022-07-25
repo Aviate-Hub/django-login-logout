4502dab922c2c5b07ca379b8660eb54814ec47e4
Nitesh Divecha <ndivecha@granitenet.com>
2022-07-25 14:07:05 -0400

    Initial commit.

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from curses.ascii import HT
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def say_hello(request):
+    return HttpResponse("Main view")
